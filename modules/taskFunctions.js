1e0c78af1f720f9ccee9095f87b199e2cf8de138
leinformat <leinformat@gmail.com>
2023-09-24 22:43:27 -0500

    List Task Step 2

--- modules/taskFunctions.js
+++ modules/taskFunctions.js
@@ -1,26 +1,18 @@
 import { tasksContainer, task } from "./domElements.js";
 
 function taskTemplate(data, clonedCard) {
-  /*
-    clonedCard.querySelector(".hubdb__image").src = values[columns.image];
-    clonedCard.querySelector(".hubdb__country").textContent = values[columns.country];
-    clonedCard.querySelector(".hubdb__team").textContent = values[columns.team]?.name;
-    clonedCard.querySelector(".hubdb__owner span").textContent = values[columns.name];
-    clonedCard.querySelector(".hubdb__description").textContent = values[columns.description];
-    */
     const assignedByImg = clonedCard.querySelector(".clickup-extension__img-asignBy");
     assignedByImg.src = data.creator.profilePicture;
     assignedByImg.alt = data.creator.username;
 
-    const taskLink = clonedCard.querySelector(".clickup-extension__task-link");
-    taskLink.href = data.url;
-    taskLink.textContent = "Ir a la Tarea";
+    const taskName = clonedCard.querySelector(".clickup-extension__task-name");
+    taskName.href = data.url;
+    taskName.textContent = data.name.slice(0, 60) + " ...";
+    taskName.title = "Go to Task";
+    taskName.target = '_blank';
 
     clonedCard.querySelector(".clickup-extension__asignBy").textContent = data.creator.username;
-    clonedCard.querySelector(".clickup-extension__task-name").textContent = data.name;
     clonedCard.querySelector(".clickup-extension__task-status").textContent = data.status.status;
-    clonedCard.querySelector(".clickup-extension__task-status").textContent = data.status.status;
-
 }
 
 // Function to handle Tasks
